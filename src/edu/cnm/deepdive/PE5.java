70c348c667aeb8d423e62cb75bcca0c595cb5cfb
Dave <davemgoldsmith@yahoo.com>
2017-09-26 12:21:17 -0600

    finished

---
+++ src/edu/cnm/deepdive/PE5.java
@@ -0,0 +1,33 @@
+package edu.cnm.deepdive;
+
+public class PE5 {
+
+	public PE5() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <= 1000000000; i++) {
+			if (((i % 1) == 0) && ((i % 2) == 0) && ((i % 3) == 0) &&
+			((i % 4) == 0) && ((i % 5) == 0) && ((i % 6) == 0) &&
+			((i % 7) == 0) && ((i % 8) == 0) && ((i % 9) == 0) &&
+			((i % 10) == 0) && ((i % 11) == 0) && ((i % 12) == 0) &&
+			((i % 13) == 0) && ((i % 14) == 0) && ((i % 15) == 0) &&
+			((i % 16) == 0) && ((i % 17) == 0) && ((i % 18) == 0) &&
+			((i % 19) == 0) && ((i % 20) == 0)
+					
+					
+					)
+			  
+		
+				System.out.println(i);
+			}
+			
+			
+			
+		}
+
+	}
+
+
